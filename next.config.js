ce98d3fe99b89347a17f991b28c799115fc4cacd
Stoopid Penguin <pinxhoes@gmail.com>
2024-08-27 21:18:09 +0200

    removed doubled frontend

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  // Remove the experimental section entirely
-};
-
-module.exports = nextConfig;
--- utils/web3.tsx
+++
@@ -1,8 +0,0 @@
-import { ethers } from "ethers";
-import { Provider, Wallet } from "zksync-ethers";
-
-export async function createWallet(): Promise<Wallet> {
-    const provider = new Provider(process.env.NEXT_PUBLIC_ABSTRACT_RPC_URL!);
-    const randomWallet = ethers.Wallet.createRandom();
-    return new Wallet(randomWallet.privateKey, provider);
-}
